710b0cecb98f01671b6509f079ee4d0b38da5971
F-ryosuke-39 <r.fujita.sys19@morijyobi.ac.jp>
2021-01-21 23:09:36 +0900

    wa

--- src/dao/FollowDao.java
+++ src/dao/FollowDao.java
@@ -207,8 +207,8 @@ public class FollowDao {
     		//後の手順で?に値を設定します。
 
     		// フォローしたユーザーの取得（自分は表示しない）
-    		String sql = "SELECT account.account_id AS account_id, account.name as account_name FROM account"
-    				+ "INNER JOIN follows ON account.account_id = follows.account_id" + "WHERE follows.account_id = ?";
+    		String sql = "SELECT follows.follow AS follow_id, account.name as account_name FROM follows"
+    				+ "INNER JOIN account ON follows.follow = account.id" + "WHERE account_id = ?";
 
     		//⑥SQLを実行するための準備(構文解析)
     		pstmt = con.prepareStatement(sql);
@@ -234,5 +234,95 @@ public class FollowDao {
             throw new RuntimeException(e);
         }
         return followList;
+    }
+
+    // 指定したuser_Idのフォロワー 一覧の取得
+    public List<account> getFollowerList(int userId) {
+        List<account> followerList = new ArrayList<account>();
+
+        //②アクセスに必要な変数の初期化
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+
+    	try{
+			//③JDBCドライバをロードする
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			//④データベースと接続する(コネクションを取ってくる)
+    		//第1引数→接続先URL
+    		//第2引数→ユーザ名
+    		//第3引数→パスワード
+    		con = DriverManager.getConnection(url, user, pw);
+
+    		//⑤SQL文の元を作成する
+    		//?をプレースホルダと言います。
+    		//後の手順で?に値を設定します。
+
+    		// フォローしたユーザーの取得（自分は表示しない）
+    		String sql = "SELECT account.id AS account_id, account.name as account_name FROM account"
+    				+ "INNER JOIN follows ON account.id = follows.account_id" + "WHERE follows.follow = ?";
+
+    		//⑥SQLを実行するための準備(構文解析)
+    		pstmt = con.prepareStatement(sql);
+
+    		//⑦プレースホルダに値を設定
+    		//第1引数→何番目の?に設定するか(1から数える)
+    		//第2引数→?に設定する値
+
+            pstmt.setInt(1, userId);
+
+            // SELECTを実行
+            try (ResultSet rs = pstmt.executeQuery()) {
+              // SELECT文の結果をArrayListに格納
+              while (rs.next()) {
+            	  String followerUserId = rs.getString("USER_ID");
+                  String followerUserName = rs.getString("USER_NAME");
+
+                  account follower = new account(followerUserId, followerUserName);
+                  followerList.add(follower);
+             }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return followerList;
+    }
+
+    // フォローを解除する
+    public void followDelete(int userId, int followId) {
+    	//②アクセスに必要な変数の初期化
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+
+    	try{
+			//③JDBCドライバをロードする
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			//④データベースと接続する(コネクションを取ってくる)
+    		//第1引数→接続先URL
+    		//第2引数→ユーザ名
+    		//第3引数→パスワード
+    		con = DriverManager.getConnection(url, user, pw);
+
+    		//⑤SQL文の元を作成する
+    		//?をプレースホルダと言います。
+    		//後の手順で?に値を設定します。
+
+            String sql = "DELETE FROM follows WHERE account_id = ? AND follow = ?";
+
+            //⑥SQLを実行するための準備(構文解析)
+    		pstmt = con.prepareStatement(sql);
+
+            pstmt.setInt(1, userId);
+            pstmt.setInt(2, followId);
+
+           // INSERT文を実行
+           int result = pstmt.executeUpdate();
+           if (result != 1) {
+                    throw new RuntimeException("フォロー解除に失敗しました。");
+           }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
